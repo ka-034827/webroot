313d7235bfe6738e7f9255c70a26d4060aca6d35
ka-034827 <ka-034827@users.noreply.github.com>
2017-03-14 20:21:32 +0530

    Changed dbBuild script.

--- dbBuild.sh
+++ dbBuild.sh
@@ -1,9 +1,5 @@
 #!/run/current-system/sw/bin/bash
 
-SRDFDIR='/webroot'			# Direcotry for srdf application execution
-
-cd $SRDFDIR				# Changing over to application directory
-
 sqlite3 srdfData.db < srdfDBSchema.sql	# Setting up Sqlite Database
 
 
